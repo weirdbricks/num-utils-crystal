afbb1d27a03bd403bed4287bab9757891cb70139
Lampros <weirdbricks@users.noreply.github.com>
2021-10-23 12:19:26 -0700

    Create numsum-crystal.cr

---
+++ numsum-crystal.cr
@@ -0,0 +1,53 @@
+##############################################
+# Takes numbers through STDIN and summarizes
+# them, just like `numsum` from num-utils
+##############################################
+
+##############################################
+# Starting values
+##############################################
+sum=0
+input = STDIN.gets_to_end
+item_integer = 0
+item_float   = 0.0
+is_integer=false
+
+# Check if we have any newlines
+if input.includes?("\n") 
+  array_of_strings = input.split("\n")
+else
+  abort "no new lines detected - cannot add anything."
+end
+
+# If any of the array's items are empty, delete them
+# from the array
+array_of_strings.delete("")
+
+# Go through the array of strings and try to convert
+# the items to integers
+array_of_strings.each do |item|
+  # first try to convert to an integer
+  begin
+    item_integer = item.to_i
+    sum=sum+item_integer
+    is_integer=true
+  rescue
+    is_integer=false
+  end
+
+  # if the integer conversion failed, let's see if we can convert to a float
+  if is_integer == false
+    # then try converting to a float
+    # if that fails, set number to 0
+    begin
+      item_float = item.to_f
+      sum=sum+item_float
+    rescue
+      puts "\"#{item}\" is not a float - Treating as 0"
+      sum=sum+0
+    end
+  end
+end
+
+# finally print out the sum :)
+puts sum
